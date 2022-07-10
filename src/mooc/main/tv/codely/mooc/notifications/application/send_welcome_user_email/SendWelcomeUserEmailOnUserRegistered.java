33046da724f6fe080e33e94061d895497f9c9841
Francisco Williams <fwmcomputer@gmail.com>
2022-07-10 17:56:09 +0200

    Add BC retention for notifications

--- src/mooc/main/tv/codely/mooc/notifications/application/send_welcome_user_email/SendWelcomeUserEmailOnUserRegistered.java
+++
@@ -1,4 +0,0 @@
-package tv.codely.mooc.notifications.application.send_welcome_user_email;
-
-public class SendWelcomeUserEmailOnUserRegistered {
-}
---
+++ src/retention/main/tv/codely/retention/email/application/send_welcome_user_email/SendWelcomeUserEmailOnUserRegistered.java
@@ -0,0 +1,16 @@
+/*package tv.codely.retention.email.application.send_welcome_user_email;
+
+import tv.codely.mooc.courses.domain.CourseCreatedDomainEvent;
+import tv.codely.shared.domain.Service;
+import tv.codely.shared.domain.bus.event.DomainEvent;
+import tv.codely.shared.domain.bus.event.DomainEventSubscriber;
+
+import java.lang.annotation.Annotation;
+
+@Service
+@DomainEventSubscriber({CourseCreatedDomainEvent.class})
+public class SendWelcomeUserEmailOnUserRegistered {
+
+
+}
+*/
